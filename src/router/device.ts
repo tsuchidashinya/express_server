824025a4d6b914c1b9f7b3c15196b0c1cee24af5
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2023-11-03 12:46:12 +0900

    fix: 空の関数実装

--- src/router/device.ts
+++ src/router/device.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getAllDevice, getDeviceSearch, createDevice, autoCreateDevice } from '../controller/device'
+import { getAllDevice, getDeviceSearch, createDevice, autoCreateDevice, deleteDevice } from '../controller/device'
 
 const router = express.Router()
 router.get('/', getAllDevice)
@@ -9,5 +9,7 @@ router.get('/search', getDeviceSearch)
 router.post('/', createDevice)
 
 router.post('/auto-data', autoCreateDevice)
+
+router.delete('/', deleteDevice)
 
 export default router
